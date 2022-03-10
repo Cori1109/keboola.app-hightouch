5689981259d44e5dfd1e0732183a35e1f584d421
Cori1109 <chris.jenkins.j1005@gmail.com>
2022-03-09 21:33:04 -0500

    fixing build errors

--- src/hightouch/client.py
+++ src/hightouch/client.py
@@ -9,11 +9,9 @@ class hightouchClient(HttpClient):
     Main class for the Hightouch Client.
     """
 
-    
     def __init__(self, access_token):
         auth_header = {'Authorization': f'Bearer {access_token}'}
         super().__init__(BASE_URL, auth_header=auth_header)
-
 
     def run_sync(self, sync_id):
         """
